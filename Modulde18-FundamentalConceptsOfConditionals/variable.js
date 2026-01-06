8f8de66ef852253147dda71e221cb1a08ad911a0
refathosen <refatutsab29@gamil.com>
2026-01-06 17:24:01 +0600

    module18 started

---
+++ Modulde18-FundamentalConceptsOfConditionals/variable.js
@@ -0,0 +1,16 @@
+// variable => container to store data
+
+//number
+let applePrice = 500;
+
+//string
+let departmentName = "Mathematics";
+
+//boolean
+let isPassed = true;
+
+//undefined
+let studentAge;
+
+//null
+let selectedCourse = null;
